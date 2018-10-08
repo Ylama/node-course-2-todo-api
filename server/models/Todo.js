ed3c5c907b46aae1295eeb3c79ce9ff87c87630f
Chris Viljoen <chris@p2.group>
2018-10-08 15:04:19 +0200

    Test POST /todos route

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,32 +1,20 @@
-var mongoose = require('mongoose')
+var mongoose = require('mongoose');
 
 var Todo = mongoose.model('Todo', {
-    text: {
-        type: String,
-        require: true,
-        minlength: 1,
-        trim: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    },
-    completedAt: {
-       type: Number,
-       default: null
-    }
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  completedAt: {
+    type: Number,
+    default: null
+  }
 });
-
-//var newTodo = new Todo({
-//    text: 'true',
-//    completed: true,
-//    completedAt: 153522
-//});
-//
-//newTodo.save().then((doc) => {
-//    console.log('Saved the todo', doc);
-//}, (e) => {
-//    console.log('Unable to save todo');
-//});
 
 module.exports = {Todo};
--- server/models/User.js
+++ server/models/User.js
@@ -1,23 +1,12 @@
 var mongoose = require('mongoose');
 
-var Users = mongoose.model('Users', {
-   email: {
-       type: String,
-       required: true,
-       trim: true,
-       minlength: 1
-   } 
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 1
+  }
 });
 
-//var newUsers = new Users({
-//    email: 'chrisieviljoen@yahoo.com'
-//});
-//
-//newUsers.save().then((doc) => {
-//    console.log('Saved the user: ', doc)
-//},(e) => {
-//    console.log('Unable to save', e)
-//});
-
-
-module.exports = {Users};
+module.exports = {User}
