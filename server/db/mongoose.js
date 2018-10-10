2ffe4807f886b85462f6a3efbbab20987dd1fd33
Chris Viljoen <chris@p2.group>
2018-10-10 07:37:30 +0200

    add setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://test:tester1@ds157469.mlab.com:57469/todo-app-api'
+  mlab: 'mongodb://new:apptodo1@ds157469.mlab.com:57469/todo-app-api'
     
 };
 mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
